fefac972c97a982662b731208e7498dd1ef482d7
Óscar Rodrigues <oscarmsrodrigues@gmail.com>
2015-03-23 15:37:01 +0000

    added Faders
    Fader display now working

--- src/Fader.cpp
+++ src/Fader.cpp
@@ -16,13 +16,45 @@ Fader::Fader(string title, float initX, float initY, int extend)
 }
 
 float Fader::getValue() {
+    usage = false;
+    if (ofDist(ofGetMouseX(), ofGetMouseY(), fadePos, zeroY)<fadeRadius*3) {
+        if (ofGetMousePressed()) {
+            usage = true;
+            faderVal = ofMap(ofGetMouseX(), zeroX, zeroX+leng, low, high) ;
+            if (faderVal<low) faderVal=low;
+            if (faderVal>high) faderVal =high;
+        }
+    }
+    return(faderVal);
     
 }
 
 void Fader::keySupport(bool type) {
     
+    if (type) {
+        faderVal+=0.05;
+        if (faderVal<low) faderVal=low;
+        if (faderVal>high) faderVal =high;
+    }
+    else {
+        faderVal-=0.05;
+        if (faderVal<low) faderVal=low;
+        if (faderVal>high) faderVal =high;
+    }
+    
 }
 
 void Fader::display(){
+    
+    ofColor(255);
+    ofLine(zeroX, zeroY, zeroX+leng, zeroY);
+    fadePos = ofMap(getValue(), low, high, zeroX, zeroX+leng);
+    ofRect(fadePos-5,zeroY-5,10,10);
+// TODO: check  text
+    //textAlign(LEFT,TOP);
+    //textSize(10);
+    //text(myTitle, zeroX, zeroY+10);
+    
+    ofDrawBitmapString(myTitle, zeroX, zeroY+10);
     
 }
