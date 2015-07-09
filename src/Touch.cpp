a7b1805338be56db0072b088e0730d86c6b22a6c
Óscar Rodrigues <oscarmsrodrigues@gmail.com>
2015-07-09 15:28:41 +0100

    Fixes #37
    Rebuilds particle design, so that they are formed inside out, instead
    of having a drawn line displaying increment

--- src/Touch.cpp
+++ src/Touch.cpp
@@ -26,7 +26,7 @@ void Touch::draw() {
     if (y > ofApp::maxParticleY || y < CONSOLE_HEIGHT) {
         ofSetHexColor(PARTICLE_LIMIT_COLOR);
     }
-    
+ /*
     ofSetLineWidth(TOUCH_LINE_WIDTH);
     ofNoFill();
     
@@ -36,7 +36,7 @@ void Touch::draw() {
     float angleEnd = float(increment)*(360./float(TOUCH_MAX)) - 180;
     polyline.arc(pt, TOUCH_RADIUS, TOUCH_RADIUS, angleBegin, angleEnd, ARC_RESOLUTION);
     polyline.draw();
-    
+*/
     // TODO: check these magic numbers when rebuilding code according to screen size
     ofFill();
     ofEllipse(x, y, increment*3, increment*3);
